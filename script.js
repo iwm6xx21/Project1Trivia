fd1e569447c400d0ddfe7ebbf9c32038b7b93d38
ispoon21 <isaiahweathersp@gmail.com>
2021-11-09 18:22:22 -0700

    project vs3

--- script.js
+++ script.js
@@ -6,6 +6,13 @@
 const startButton = document.getElementById("startGame-btn")
 const questionAndAnswers = document.getElementById("questionsAndAnswers")
 const grabQuestion = document.getElementById("questions")
+const grabAnswers = document.getElementById('button-choices')
+let questionsLoop, currentIndex
+
+
+grabAnswers.addEventListener("click", ()  => {
+    alert("Hello")
+})
 
 // Start Button Prompts
 startButton.addEventListener("click", startTheGame)
@@ -15,9 +22,13 @@ function startTheGame () {
         if(theButton === "hidden") {
             alert("Instructions: You must answer at least 4/5 questions correctly (80%) to win the game! Click Ok to get started. Let's play!")
         questionAndAnswers.style.visibility = "visible"
-
         //Test Code
-        grabQuestion.innerHTML = questionsAsked[0].question
+        questionsLoop = questionsAsked.sort(() => Math.random() - .5 )
+        
+        currentIndex = 0
+        nextQuestion()
+
+        
     }
 }
 
@@ -34,22 +45,59 @@ function startTheGame () {
 
 // questions
 
+// function shuffleQuestions(array) {
+//     let currentIndex = array.length, randomIndex
+//     while (currentIndex !=0) {
+//         randomIndex = Math.floor(Math.random() * currentIndex);
+//         currentIndex--;
+//         [array[currentIndex], array[randomIndex]] = [array[randomIndex]], array[currentIndex]
+//     }
+//     return array;
+// }
+
 const questionsAsked = [
-    {question: "What is the blank?", answer:[
-        {choice1: "5", right: false}, {choice2: "9", right:true}, {choice3: "10", right: false}
-    ]},
+    {question: "Before ‘Curb Your Enthusiasm’ what show did Larry David write?", answer:[
+        {choice: "Friends", right: false}, {choice: "Seinfeld", right:true}, {choice: "Arrested Development", right: false}
+        ]
+    },
+    {question: "What is Larry's Ex-Wife's name on the show", answer:[
+        {choice: "Victoria", right: false}, {choice: "Susie", right:false}, {choice: "Cheryl", right: true}
+        ]
+    },
+
 ]
+
+
+
+
+function showTheQuestion (question) {
+    grabQuestion.innerHTML = question.question
+    document.querySelectorAll(".btn").forEach((btn, i) => {
+        btn.innerHTML = question.answer[i].choice
+    })
+}
+
+function answerSelected () {
+
+}
 
 
 // go to next question
 function nextQuestion () {
 
+    showTheQuestion(questionsLoop[currentIndex])
+
     //or add code that will automaticall go to next question once answer is selected. 
 
 }
 
+
 //select answer
-function selectAnswer () { 
+function showTheAnswer () { 
+    
+}
+
+function nextAnswer () {
 
 }
 
