f707039f64c7e7362e3b9e6156571474a09d9f8e
ispoon21 <isaiahweathersp@gmail.com>
2021-11-10 22:15:54 -0700

    project vs4

--- script.js
+++ script.js
@@ -6,13 +6,10 @@
 const startButton = document.getElementById("startGame-btn")
 const questionAndAnswers = document.getElementById("questionsAndAnswers")
 const grabQuestion = document.getElementById("questions")
-const grabAnswers = document.getElementById('button-choices')
+// const grabAnswers = document.getElementById('button-choices')
 let questionsLoop, currentIndex
 
 
-grabAnswers.addEventListener("click", ()  => {
-    alert("Hello")
-})
 
 // Start Button Prompts
 startButton.addEventListener("click", startTheGame)
@@ -24,7 +21,7 @@ function startTheGame () {
         questionAndAnswers.style.visibility = "visible"
         //Test Code
         questionsLoop = questionsAsked.sort(() => Math.random() - .5 )
-        
+
         currentIndex = 0
         nextQuestion()
 
@@ -32,28 +29,7 @@ function startTheGame () {
     }
 }
 
-// let response = prompt ("Ready to Play?")
 
-// if (response === "yes") {
-//     alert("You must answer at least 4/5 questions correctly (80%) to win the game! Click Ok to get started. Let's play!") 
-//     document.getElementById("startGame-btn").style.visibility ="visible"
-// } else if (response === "no") {
-//     alert("Come back when you're ready to play!")
-// } else (
-//     alert("Invalid Entry")
-// )
-
-// questions
-
-// function shuffleQuestions(array) {
-//     let currentIndex = array.length, randomIndex
-//     while (currentIndex !=0) {
-//         randomIndex = Math.floor(Math.random() * currentIndex);
-//         currentIndex--;
-//         [array[currentIndex], array[randomIndex]] = [array[randomIndex]], array[currentIndex]
-//     }
-//     return array;
-// }
 
 const questionsAsked = [
     {question: "Before ‘Curb Your Enthusiasm’ what show did Larry David write?", answer:[
@@ -74,18 +50,40 @@ function showTheQuestion (question) {
     grabQuestion.innerHTML = question.question
     document.querySelectorAll(".btn").forEach((btn, i) => {
         btn.innerHTML = question.answer[i].choice
+        if (question.answer[1].right === true){
+            btn.dataset.right = question.answer[1].right
+        }
+        btn.addEventListener("click", answerSelected)
+        
     })
 }
 
+
+// const grabAnswers = document.getElementById("button-choices")
+// grabAnswers.dataset
+
+// grabAnswers.addEventListener("click", answerSelected)
+
+
 function answerSelected () {
+   
+    alert("Clicked")
 
 }
+    
+   
+        
+
+    
+
+
 
 
 // go to next question
 function nextQuestion () {
 
     showTheQuestion(questionsLoop[currentIndex])
+    
 
     //or add code that will automaticall go to next question once answer is selected. 
 
