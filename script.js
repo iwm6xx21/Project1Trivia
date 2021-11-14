b95f99ee8159df6fb3f0caf2377d8dc16ee31e51
ispoon21 <isaiahweathersp@gmail.com>
2021-11-14 15:15:09 -0700

    project vs7

--- script.js
+++ script.js
@@ -5,22 +5,35 @@ const startButton = document.getElementById("startGame-btn")
 const questionAndAnswers = document.getElementById("questionsAndAnswers")
 const grabQuestion = document.getElementById("questions")
 const nextButton = document.getElementById("next-btn")
+const answersDiv = document.getElementById("button-choices")
+const scoreStorage = document.getElementById("score-storage")
+const scores = document.getElementById("score")
+let score = 0; 
 let questionsLoop, currentIndex
+
 
 
 
 // Start Button Prompts
 startButton.addEventListener("click", startTheGame)
+nextButton.addEventListener("click", () => {
+    currentIndex++
+    nextQuestion()
+})
+
     
 function startTheGame () {
     let theButton = startButton.style.visibility ="hidden"
         if(theButton === "hidden") {
             alert("Instructions: You must answer at least 4/5 questions correctly (80%) to win the game! Click Ok to get started. Let's play!")
         questionAndAnswers.style.visibility = "visible"
+        scoreStorage.style.visibility ="visible"
         
+        //will randomize the order of questions
         questionsLoop = questionsAsked.sort(() => Math.random() - .5 )
 
         currentIndex = 0
+        scores.innerText = "0"
         nextQuestion()
 
         
@@ -58,76 +71,89 @@ const questionsAsked = [
 
 
 
-
 function showTheQuestion (question) {
     grabQuestion.innerHTML = question.question
-    document.querySelectorAll(".btn").forEach((btn, i) => {
-        btn.innerHTML = question.answer[i].choice
-        
-        let correctAnswer = question.answer[i].right
-        console.log(correctAnswer)
-        
-        btn.addEventListener("click", () => {
-            if(correctAnswer === true) {
-                alert("That's right!")
-            } else if (correctAnswer === false)
-                alert("Wrong!")
-                nextButton.style.visibility = "visible"
-                // btn.removeEventListener()
-        });
 
-        
+    //will grab choices from the 'questionsAsked' Array object and loop through them. 
+    question.answer.forEach(answers => {
+        const buttons = document.createElement('button')
+        buttons.innerText = answers.choice
+        buttons.classList.add('btn')
+        if(answers.right) {
+            buttons.dataset.right = answers.right
+           
+        }
+        buttons.addEventListener("click", selectAnswers)
+        //will append children of the 'button-choice' id created in html file
+        answersDiv.appendChild(buttons)
     })
+             
+    
 }
 
 
-// const grabAnswers = document.getElementById("button-choices")
-// grabAnswers.dataset
-
-// grabAnswers.addEventListener("click", answerSelected)
 
 
-// function answerSelected () {
-   
-//     alert("Clicked")
+function selectAnswers (e) {
+    const choiceSelected = e.target
+    const right = choiceSelected.dataset.right
+    setStatusClass(document.body, right)
+    Array.from(answersDiv.children).forEach(buttons => {
+        setStatusClass(buttons,buttons.dataset.right)
+    })
 
-// }
-    
-   
+        // statement below stops game if there are no more question to iterate through
+        if(questionsLoop.length > currentIndex + 1) {
+            nextButton.style.visibility = "visible"
+        } 
+        else {
+            startButton.innerText = "Restart Game"
+            startButton.style.visibility ="visible"
+            const TotalScore = scores.innerText = score
+            if(TotalScore >=4) {grabQuestion.innerText = `You got ${TotalScore} correct which is ${TotalScore / 5 * 100}%! 'Pretty, Pretty, Pretty Good!' Larry David would be proud. 👴🏻`;score = 0} 
+            
+            else {
+                grabQuestion.innerText = `You got ${TotalScore} correct which is ${TotalScore / 5 * 100}%. 'You, know, I'm really not that bright' Larry David would be underwhelmed.` ; score = 0
+            }
+            
+        }
         
+}
 
-    
 
 
+
+// Game Scoring Function
+function scoreTracker () {
+    scores.innerText = score +=1
+}
+
+ function setStatusClass (element, right) {
+
+    resetPage(element)
+    if(right) {
+        alert("That's correct")
+        scoreTracker()
+        console.log(score)
+    } else {
+        alert("Wrong!")
+    }
+
+ }
+
+ //will reset page once the next button is selected. 
+ function resetPage () {
+    nextButton.style.visibility = "hidden"
+    //will remove additional buttons defined in html 'btn' class
+    while(answersDiv.firstChild) {
+        answersDiv.removeChild(answersDiv.firstChild)
+    }
+}
 
 
 // go to next question
 function nextQuestion () {
-
+    resetPage ()
     showTheQuestion(questionsLoop[currentIndex])
-    
-
-    //or add code that will automaticall go to next question once answer is selected. 
 
 }
-
-
-//select answer
-function showTheAnswer () { 
-    
-}
-
-function nextAnswer () {
-
-}
-
-// Track Score
-
-function scoreTracker () {
-
-}
-
-// ("click", answerSelected)
-//         if(correctAnswer === true) {
-//             alert("That's right!")
-//         }
